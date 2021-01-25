065db478cf6eb29f7b126e86719a898041f581e5
Evander Turner <evander.rt@btinternet.com>
2021-01-25 23:14:56 +0000

    :tada: commented code

--- botbot.py
+++ botbot.py
@@ -1,3 +1,6 @@
+########### SIMPLE DEPOP RELISTING BOT ###############
+############# BY REDNAVE12  2020/21 ##################
+
 from config import keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,18 +14,6 @@ import sys
 import math
 from datetime import datetime
 
-#IMPROVEMENTS TO BE MADE:
-#ERROR HANDLING WHEN WEB ELEMENTS CAN'T BE FOUND. HELPED BY BETTER XPATH CHOICES.
-#TRY CATCHES ETC.
-#MAKE A UI?????
-#CAN WE MAKE THIS IN TO AN APP?? OR WEB APP?
-#UI SHOULD BE ABLE TO TAKE IN:
-#USERNAME
-#PASSWORD
-#INTERVAL
-#SHOW PROGRESS
-#TIME UNTIL NEXT
-
 options = Options()
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 options.add_argument('user-agent={user_agent}')
@@ -33,11 +24,12 @@ options.add_argument('--disable-gpu')
 driver = webdriver.Chrome('./chromedriver', options=options)
 actions = ActionChains(driver)
 
-#GLOBAL VARIABLES
+######## GLOBAL VARIABLES ##########
 hrefs = []
 count = 0
 lastSoldItems = []
 
+####### TO SCROLL ALL THE WAY TO THE BOTTOM OF YOUR FEED ######
 def infiniteScroll():
     SCROLL_PAUSE_TIME = 5
 
@@ -52,6 +44,7 @@ def infiniteScroll():
             break
         last_height = new_height
 
+########### OPEN DEPOP PROFILE AND LOG IN ############
 def openChrome(k):
     global driver
     driver.get(k['profile_url'])
@@ -78,6 +71,7 @@ def openChrome(k):
     
     time.sleep(5)
 
+########## FIND NUMBER OF AVAILABLE ITEMS AND GET A LIST OF LINKS TO EACH ONE ############
 def search():
     global hrefs
     hrefs.clear()
@@ -128,31 +122,38 @@ def search():
     availableItems = 0
     
     return hrefs
-    
+
+##### PRESS THE MOVE DOWN SOLD ITEMS BUTTON IF NEED BE ######
 def moveDownSoldItems():
     driver.execute_script("window.scrollTo(0, 0)")
     driver.find_element_by_xpath('//*[@id="main"]/div[1]/div[4]/div/button[1]').click()
 
+########## RELIST ITEMS BY ITERATING THROUGH LIST OF LINKS ###########
 def relist(hrefs, k): 
     global count
     driver.implicitly_wait(10)
 
+    #### CONVERT LINK OF ITEM TO THE ITEM'S EDIT PAGE, SCROLL AND CLICK SAVE CHANGES ####
     for x in hrefs : 
         x.split('products')
         hrefOfEdit = x.split('products')[0] + 'products/edit' + x.split('products')[1]
         driver.get(hrefOfEdit)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
         time.sleep(2)
-        #this is usually were we get a no such element exception
+        
+        #### this is usually where we (albeit occasionally) get a no such element exception ####
         driver.find_element_by_xpath('//*[@id="main"]/form/div[2]/button[1]').click()
         time.sleep(5)
         count += 1
    
     driver.get(k['profile_url'])
-        
+     
+##### MAIN FUNCTION ######     
 if __name__ == '__main__':
+    ### OPEN PROFILE & LOG IN ###
     openChrome(keys)
 
+    #### INFINITELY LOOP THROUGH SEARCHING AND RELISTING ANYWHERE BETWEEN 45-90 MINS ####
     while True:
         search()
         driver.refresh()
